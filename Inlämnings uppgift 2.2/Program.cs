5863eb69ac4bf1aa99f6b8b1db80428a89384424
ahka07001 <ahka07001@E-PW05CGQT>
2024-10-03 14:00:17 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inlämnings_._uppgift_._2_._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin, mätt i meter?");
+            string Elin = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma, mätt i meter?");
+            string Alma = Console.ReadLine();
+            int x = int.Parse(Elin);
+            int y = int.Parse(Alma);
+            int skillnad = x-y;
+            
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma.");
+
+
+        }
+    }
+}
